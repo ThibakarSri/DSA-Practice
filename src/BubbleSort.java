791e07e4f058ef6c4e81a1508b7b52b4b486f590
ThibakarSri <thibakarsri33@gmail.com>
2026-01-12 12:39:00 +0530

    Practice Bubble sort method

---
+++ src/BubbleSort.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class BubbleSort {
+    public static void main (String []args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the length of the array: ");
+        int n = input.nextInt();
+        int [] arr = new int [n];
+        for (int i=0; i<n; i++){
+            arr[i] = input.nextInt();
+        }
+
+        System.out.println("Before sorting: ");
+
+        for(int value: arr){
+            System.out.println(value);
+        }
+        System.out.println();
+
+        // Sorting process
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n-1-i; j++){
+                if(arr[j] > arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] =arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+
+        System.out.println("After sorting: ");
+        for(int value: arr){
+            System.out.println(value);
+        }
+    }
+}
