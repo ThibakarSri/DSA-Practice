849627d720cc42e38790b07e97a6fdb83c47230f
ThibakarSri <thibakarsri33@gmail.com>
2026-01-26 00:46:38 +0530

    Practice Rotate Array question

---
+++ src/RotateArray.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+public class RotateArray {
+
+    public static void rotateRight(int[] arr, int k) {
+        int n = arr.length;
+        k = k % n; // Handle k > n
+
+        reverse(arr, 0, n - 1);
+        reverse(arr, 0, k - 1);
+        reverse(arr, k, n - 1);
+    }
+
+    private static void reverse(int[] arr, int start, int end) {
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        int k = 2;
+
+        rotateRight(arr, k);
+        System.out.println(Arrays.toString(arr));
+    }
+}
