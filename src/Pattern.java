f969c5f741bfd45041ae9141ee9f91607ad46d1e
ThibakarSri <thibakarsri33@gmail.com>
2026-02-06 23:50:43 +0530

    Practice on patterns

--- src/Pattern.java
+++ src/Pattern.java
@@ -16,7 +16,47 @@ public class Pattern {
             }
             System.out.println();
         }
+
+
+        System.out.println();
+        for(int i=1; i<=lines; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+
+
+        System.out.println();
+        for(int i=1; i<=lines; i++){
+            for(int j=1; j<=lines-i; j++){
+                System.out.print(" ");
+            }
+            for(int j=1; j<=i; j++){
+                System.out.print(j+ " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+        int num =1;
+        for(int i=1; i<=lines; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print(num++ + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+        for(int i=1; i<=lines; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print(i++ + " ");
+            }
+        }
     }
+
+
 }
+
 
 
